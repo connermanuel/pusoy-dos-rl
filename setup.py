25a386b61b4f04eddcb1fc703497f61a398cdc61
connermanuel <manuel.conner.g@gmail.com>
2024-04-24 13:25:02 +0800

    actual src

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
    name='pusoy',
-   packages=['pusoy'],
-   package_dir={'pusoy': 'pusoy/src'}
+   packages=find_packages("src"),
+   package_dir={'': 'src'},
 )
